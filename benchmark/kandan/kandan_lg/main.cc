f245e4c043e38748811575c3dfd1608b26c42664
Mingwei Samuel <mingwei.samuel@gmail.com>
2020-03-09 21:30:22 -0700

    regen

--- benchmark/kandan/kandan_lg/main.cc
+++ benchmark/kandan/kandan_lg/main.cc
@@ -6,7 +6,7 @@
 int main() {
   read_data();
     Query0Result q_0_result;
-     query_0_plan_0(35780, q_0_result);
+     query_0_plan_0(51278, q_0_result);
     Query1Result q_1_result;
      query_1_plan_1(q_1_result);
 
